bd9194b6fd8f96b7aef2107202ec395cd03f1b71
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-10-20 14:22:44 +0300

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+function showMenu() {
+  document.querySelector('.header__menu').style.display = 'block';
+}
+
+document.querySelector('.header__menu-button')
+  .addEventListener('click', showMenu);
