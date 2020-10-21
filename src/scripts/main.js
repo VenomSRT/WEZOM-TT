9250f6e76dad6992f3552e75a0d90f77d250cffc
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-10-21 10:31:50 +0300

    second commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,16 @@
 'use strict';
 
-function showMenu() {
-  document.querySelector('.header__menu').style.display = 'block';
+let menuDisplay = 'none';
+
+function toggleMenu() {
+  if (menuDisplay === 'none') {
+    document.querySelector('.header__menu').style.display = 'block';
+    menuDisplay = 'block';
+  } else {
+    document.querySelector('.header__menu').style.display = 'none';
+    menuDisplay = 'none';
+  }
 }
 
 document.querySelector('.header__menu-button')
-  .addEventListener('click', showMenu);
+  .addEventListener('click', toggleMenu);
