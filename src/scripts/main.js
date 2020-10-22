c2270ed8078dc23109b93751c498c9de59c8b484
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-10-23 01:42:45 +0300

    commit 3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,3 +14,17 @@ function toggleMenu() {
 
 document.querySelector('.header__menu-button')
   .addEventListener('click', toggleMenu);
+
+document.querySelector('.labels-container')
+  .addEventListener('click', (event) => {
+    if (event.target.tagName === 'LABEL') {
+      const elemClass = event.target.className;
+
+      document.querySelectorAll('.goods-filter__form')
+        .forEach(elem => {
+          elem.style.display = 'none';
+        });
+
+      document.getElementById(elemClass.split('--')[1]).style.display = 'flex';
+    }
+  });
